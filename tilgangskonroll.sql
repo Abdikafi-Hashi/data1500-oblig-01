1a4be1859def5fc69f27ef5014f9fe6ffac777ca
Abdikafi-Hashi02 <abdikafiabdullahi2002@gmail.com>
2026-03-08 17:30:38 +0100

    db.oblig1

--- tilgangskonroll.sql
+++ tilgangskonroll.sql
@@ -9,9 +9,10 @@ CREATE USER kunde_1 WITH PASSWORD 'passord123';
 
 GRANT kunde TO kunde_1;
 
-CREATE VIEW kunde_utleier AS
-SELECT *
-FROM utleie
-WHERE kunde_id = 1;
+CREATE VIEW mine_utleier AS
+SELECT u.*
+FROM utleie u
+JOIN kunde k ON u.kunde_id = k.kunde_id
+WHERE k.epost = current_user;
 
-GRANT SELECT ON kunde_utleier TO kunde;
+GRANT SELECT ON mine_utleier TO kunde;
